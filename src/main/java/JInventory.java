566336e5e4579d0994400bdb07b568a40915db71
Tomas <tomasrodcas@gmail.com>
2021-04-23 17:16:34 -0400

    Probando Merge (Nueva funcion suma)

--- src/main/java/JInventory.java
+++ src/main/java/JInventory.java
@@ -2,5 +2,11 @@ public class JInventory {
 
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+        int b = sumar(5, 6);
+        System.out.println(b);
+    }
+
+    public static int sumar(int a, int b){
+        return a+b;
     }
 }
