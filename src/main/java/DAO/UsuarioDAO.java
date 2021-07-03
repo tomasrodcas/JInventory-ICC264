d9552034972857a03bc83a3669a9064ce84ba0e3
loslas98 <m.divin.u@gmail.com>
2021-07-03 19:52:00 -0400

    se agrega paquete DAO

---
+++ src/main/java/DAO/UsuarioDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+import DTO.UsuarioDTO;
+
+public class UsuarioDAO {
+    public void addUser(UsuarioDTO usuario){
+
+    }
+    public void editUserById(UsuarioDTO usuario, int id){
+
+    }
+
+}
+}
