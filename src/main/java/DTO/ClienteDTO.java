e0be5f1d8e640705ab806075a29c56645eae3fbc
Felpo123 <felpokun123@gmail.com>
2021-07-03 21:28:19 +0200

    Commit ClienteDTO, ProveedorDTO, UsuarioDTO

--- src/main/java/DTO/ClienteDTO.java
+++ src/main/java/DTO/ClienteDTO.java
@@ -1,4 +1,29 @@
 package DTO;
 
 public class ClienteDTO {
+
+    private final String nombre;
+    private final String email;
+    private final int telefono;
+    private final int rut;
+
+    public ClienteDTO(String nombre, String email, int telefono, int rut){
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono  = telefono;
+        this.rut = rut;
+
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+    public int getRut(){
+        return this.rut;
+    }
 }
