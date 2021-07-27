e5a8969513a3075a98803a81ddf8ddeacd8ed7d7
tomasrodcas <tomasrodcas@gmail.com>
2021-07-27 02:58:31 -0400

    Readme y Base de datos

--- database/jinventoryTest.sql
+++ database/jinventoryTest.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-07-2021 a las 05:35:49
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.5
+-- Tiempo de generación: 27-07-2021 a las 05:37:57
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
   `nombre` varchar(80) NOT NULL,
-  `rut` int(11) NOT NULL,
+  `rut` varchar(13) NOT NULL,
   `email` varchar(40) DEFAULT NULL,
   `telefono` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,8 +40,11 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id`, `nombre`, `rut`, `email`, `telefono`) VALUES
-(1, 'Alejandro Uribe', 20079853, 'asd@asd.com', 12212212),
-(2, 'Tomas Rodriguez', 20079829, 'tomasrodcas@gmail.com', 11111111);
+(1, 'Cliente 1', '20079853-8', 'cliente1@clientes.com', 222222221),
+(2, 'Cliente 2', '20079829-5', 'cliente2@clientes.com', 222222211),
+(3, 'Cliente 3', '7051267-k', 'cliente3@clientes.com', 222222111),
+(5, 'Cliente 4', '76523553-7', 'cliente4@clientes.com', 222221111),
+(6, 'Cliente 5', '5365610-2', 'cliente5@clientes.com', 222211111);
 
 -- --------------------------------------------------------
 
@@ -55,17 +58,19 @@ CREATE TABLE `items` (
   `cantidad` int(11) NOT NULL,
   `precio` int(11) NOT NULL,
   `marca` varchar(40) NOT NULL,
-  `rut_proveedor` int(11) NOT NULL
+  `id_proveedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `items`
 --
 
-INSERT INTO `items` (`id`, `nombre`, `cantidad`, `precio`, `marca`, `rut_proveedor`) VALUES
-(1, 'iphone', 300, 1000, 'Apple', 20079829),
-(2, 'Teclado RK61', 100, 69990, 'RoyalKludge', 20079853),
-(3, 'Audifonos G935', 10, 150000, 'Logitech', 7051267);
+INSERT INTO `items` (`id`, `nombre`, `cantidad`, `precio`, `marca`, `id_proveedor`) VALUES
+(1, 'Iphone XR', 295, 499990, 'Apple', 1),
+(2, 'Teclado RK61', 86, 69990, 'RoyalKludge', 2),
+(3, 'Audifonos G935', 0, 150000, 'Logitech', 3),
+(4, 'Ryzen 5600X', 99, 276000, 'Ryzen', 1),
+(6, '8 Port Desktop Switch', 90, 15990, 'Mercusys', 4);
 
 -- --------------------------------------------------------
 
@@ -74,7 +79,7 @@ INSERT INTO `items` (`id`, `nombre`, `cantidad`, `precio`, `marca`, `rut_proveed
 --
 
 CREATE TABLE `login_attempts` (
-  `usuario` varchar(80) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `login_attempts` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -82,9 +87,12 @@ CREATE TABLE `login_attempts` (
 -- Volcado de datos para la tabla `login_attempts`
 --
 
-INSERT INTO `login_attempts` (`usuario`, `login_attempts`) VALUES
-('admin', 0),
-('tomas', 0);
+INSERT INTO `login_attempts` (`id_usuario`, `login_attempts`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0);
 
 -- --------------------------------------------------------
 
@@ -95,7 +103,7 @@ INSERT INTO `login_attempts` (`usuario`, `login_attempts`) VALUES
 CREATE TABLE `proveedores` (
   `id` int(11) NOT NULL,
   `nombre` varchar(80) NOT NULL,
-  `rut` int(11) NOT NULL,
+  `rut` varchar(13) NOT NULL,
   `email` varchar(40) NOT NULL,
   `telefono` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -105,9 +113,11 @@ CREATE TABLE `proveedores` (
 --
 
 INSERT INTO `proveedores` (`id`, `nombre`, `rut`, `email`, `telefono`) VALUES
-(1, 'Proveedor 1', 20079829, 'tomasrodcas@gmail.com', 922222222),
-(2, 'Proveedor 2', 20079853, 'asd@asd.com', 999999999),
-(3, 'Proveedor 3', 7051267, 'asdasd@asdasd.com', 222222222);
+(1, 'Proveedor 1', '20079829-5', 'proveedor1@proveedores.com', 111111112),
+(2, 'Proveedor 2', '20079853-4', 'proveedor2@proveedores.com', 111111122),
+(3, 'Proveedor 3', '7051267-k', 'proveedor3@proveedores.com', 111111222),
+(4, 'Proveedor 4', '15492706-9', 'proveedor4@proveedores.com', 111112222),
+(5, 'Proveedor 5', '5365610-2', 'proveedor5@proveedores.com', 111122222);
 
 -- --------------------------------------------------------
 
@@ -129,7 +139,10 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `tipo_usuario`) VALUES
 (1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1),
-(2, 'Tomas Rodriguez', 'tomas', 'f10e2821bbbea527ea02200352313bc059445190', 0);
+(2, 'Tomas Rodriguez', 'tomas', 'f10e2821bbbea527ea02200352313bc059445190', 0),
+(3, 'Antonia Millan', 'anto', 'f10e2821bbbea527ea02200352313bc059445190', 0),
+(4, 'Maximiliano Divin', 'max', 'f10e2821bbbea527ea02200352313bc059445190', 0),
+(5, 'Felipe Castro', 'felipe', 'f10e2821bbbea527ea02200352313bc059445190', 0);
 
 -- --------------------------------------------------------
 
@@ -143,7 +156,7 @@ CREATE TABLE `ventas` (
   `id_producto` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `total` int(11) NOT NULL,
-  `rut_cliente` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -151,9 +164,12 @@ CREATE TABLE `ventas` (
 -- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`id`, `producto`, `id_producto`, `cantidad`, `total`, `rut_cliente`, `fecha`) VALUES
-(5, 'Teclado RK61', 2, 20, 100000, 20079829, '2021-07-04'),
-(6, 'Audifonos G935', 3, 2, 300000, 20079853, '2021-06-09');
+INSERT INTO `ventas` (`id`, `producto`, `id_producto`, `cantidad`, `total`, `id_cliente`, `fecha`) VALUES
+(18, 'Iphone XR', 1, 2, 999980, 2, '2021-07-26'),
+(19, 'Ryzen 5600X', 4, 1, 276000, 2, '2021-07-26'),
+(20, '8 Port Desktop Switch', 6, 10, 159900, 5, '2021-07-26'),
+(21, 'Teclado RK61', 2, 1, 69990, 3, '2021-07-26'),
+(22, 'Teclado RK61', 2, 2, 139980, 1, '2021-07-26');
 
 --
 -- Índices para tablas volcadas
@@ -172,13 +188,14 @@ ALTER TABLE `clientes`
 -- Indices de la tabla `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_proveedor` (`id_proveedor`);
 
 --
 -- Indices de la tabla `login_attempts`
 --
 ALTER TABLE `login_attempts`
-  ADD PRIMARY KEY (`usuario`);
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- Indices de la tabla `proveedores`
@@ -200,7 +217,9 @@ ALTER TABLE `usuarios`
 -- Indices de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_producto` (`id_producto`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -210,31 +229,54 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_producto` FOREIGN KEY (`id_producto`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
